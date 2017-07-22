e35e3fc43964bd426550291c08a796a7a134a495
Kevin Rodriguez <darkndream@hotmail.com>
2017-07-22 08:53:34 -0600

    Added route for brand logo, moved loading logic to ngOnInit for charactes component, temporary disabled google font because in unversity the site is blocked, added scss variables for frontpage style

--- src/app/information/components/characters/characters.component.ts
+++ src/app/information/components/characters/characters.component.ts
@@ -1,21 +1,17 @@
-import { Component, OnInit, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { InformationViewCycle } from '../information-view-cycle';
-
 import { SWApiService } from '../../services/swapi.service';
 
 import { IPeople } from '../../models/IPeople';
 import { IAlert } from '../../models/IAlert';
-
-import * as $ from 'jquery';
-import "datatables.net";
 
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
   styleUrls: ['../frontpage/frontpage.component.scss','./characters.component.scss']
 })
-export class CharactersComponent extends InformationViewCycle implements AfterViewInit {
+export class CharactersComponent extends InformationViewCycle {
 
   public characters: Array<IPeople> = [];
   public alert: IAlert = {
@@ -24,25 +20,17 @@ export class CharactersComponent extends InformationViewCycle implements AfterVi
     type: 'info'
   };
 
-  constructor(
-    private swapiService: SWApiService,
-    private cdRef: ChangeDetectorRef
-  ) { 
+  constructor(private swapiService: SWApiService,) { 
     super(); 
   }
 
   ngOnInit() {
     super.ngOnInit();
-  }
-
-  ngAfterViewInit() {
     this.characters = this.swapiService.GetAllCharacters();
     if (this.characters.length <= 0) {
       this.alert.message = "No se han encontrado resultados <i class='fa fa-cog fa-spin'></i>";
       this.alert.type = "warning";
     }
-    this.cdRef.detectChanges();
-    $('#charactersTable').DataTable();
   }
 
 }
